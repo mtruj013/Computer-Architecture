fa661baef0d8ac83a217cc5a4177dc10d7c17001
Maria Trujillo <mptruj015@gmail.com>
2020-10-15 21:51:00 +0200

    implemented multiply

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,6 +6,7 @@ import sys
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -19,24 +20,35 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
+        
         address = 0
 
-        # For now, we've just hardcoded a program:
+        if len(sys.argv) != 2:
+            print(f"usage: {sys.argv[0]} filename")
+            sys.exit(1)
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    line = line.strip()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    if line == '' or line[0] == "#": 
+                        continue
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    try:
+                        str_value = line.split("#")[0]
+                        value = int(str_value, 2)
+
+                    except ValueError:
+                        print(f"Invalid number: {str_value}")
+                        sys.exit(1)
+                    
+                    self.ram[address] = value
+                    address += 1
+        
+        except FileNotFoundError:
+            print(f"file not found: {sys.argv[1]}")
+            sys.exit(2)
+
 
 
     def alu(self, op, reg_a, reg_b):
@@ -45,8 +57,12 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            # Multiply the values in two registers together and store the result in registerA.
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+
 
     def trace(self):
         """
@@ -92,13 +108,12 @@ class CPU:
                 reg_num = operand_a
                 print(self.reg[reg_num])
                 self.pc += 2
+            elif instruction == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
             else:
                 print(f"unknown instruction {instruction} at address {self.pc}")
                 sys.exit(1)
-
-
-
-
     
     def ram_read(self, address):
         # accept the address to read and return the value stored
